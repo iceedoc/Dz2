38f6c4843b8a81cdf98aae61e752f5bc06d94772
iceedoc <paashtetik@gmail.com>
2023-12-06 15:08:40 +0300

    DZ2

---
+++ src/Main.java
@@ -0,0 +1,72 @@
+public class Main {
+    public static void main(String[] args) {
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+        dog = 8.0 + 4;
+        cat = 3.6 + 4;
+        paper = 763789 + 4;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+        dog = 8.0 - 4;
+        cat = 3.6 - 4;
+        paper = 763789 - 4;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+        var friend = 19;
+        System.out.println(friend);
+        friend = 19 + 2;
+        System.out.println(friend);
+        friend = 21 / 7;
+        System.out.println(friend);
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = 3.5 * 10;
+        System.out.println(frog);
+        frog = 35.0 / 3.5;
+        System.out.println(frog);
+        frog = 10.0 + 4;
+        System.out.println(frog);
+        var firstBoxer = 78.2;
+        System.out.println(firstBoxer);
+        var secondBoxer = 82.7;
+        System.out.println(secondBoxer);
+        var sumOfBoxers = 78.2 + 82.7;
+        System.out.println(sumOfBoxers);
+        var boxerDifference = 82.7 - 78.2;
+        System.out.println(boxerDifference);
+        var differenceBetweenAthletes = 82.7 - 78.2;
+        System.out.println(differenceBetweenAthletes);
+        differenceBetweenAthletes = 82.7 % 78.2;
+        System.out.println(differenceBetweenAthletes);
+        var generalHours = 640;
+        System.out.println(generalHours);
+        var employees = 640 / 8;
+        System.out.println(employees);
+        employees = 80 + 94;
+        System.out.println(employees);
+        employees = 174 / 8;
+        System.out.println(employees);
+        System.out.println("Спасибо за внимание");
+
+
+
+
+
+
+
+
+
+    }
+}
+
+
+
+
+
+
